f740024311907642424d8f473341608a5d557bca
Priscila Oliveira <poliveira13@hotmail.com>
2020-08-11 19:06:13 -0300

    component header

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -2,9 +2,16 @@ import React from "react";
 
 import styles from "./styles";
 import { View } from "react-native";
+import PageHeader from "../../components/PageHeader";
 
 function Favorites() {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <View style={styles.container}>
+        <PageHeader title="Meus proffys favoritos" />
+      </View>
+    </View>
+  );
 }
 
 export default Favorites;
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -2,9 +2,14 @@ import React from "react";
 
 import styles from "./styles";
 import { View } from "react-native";
+import PageHeader from "../../components/PageHeader";
 
 function TeacherList() {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <PageHeader title="Proffys disponíveis" />
+    </View>
+  );
 }
 
 export default TeacherList;

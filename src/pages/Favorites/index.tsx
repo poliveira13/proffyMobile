3c31488d3a90156084b07af23ce2b32455c3ad6e
Priscila Oliveira <poliveira13@hotmail.com>
2020-08-11 18:45:02 -0300

    paginas internas e routes

---
+++ src/pages/Favorites/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import styles from "./styles";
+import { View } from "react-native";
+
+function Favorites() {
+  return <View style={styles.container}></View>;
+}
+
+export default Favorites;
--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -17,6 +17,10 @@ function Landing() {
     navigate("GiveClasses");
   }
 
+  function handleNavigateToStudyPage() {
+    navigate("Study");
+  }
+
   return (
     <View style={styles.container}>
       <Image source={landingImg} style={styles.banner} />
@@ -27,7 +31,10 @@ function Landing() {
       </Text>
 
       <View style={styles.buttonsContainer}>
-        <RectButton style={[styles.button, styles.buttonPrimary]}>
+        <RectButton
+          onPress={handleNavigateToStudyPage}
+          style={[styles.button, styles.buttonPrimary]}
+        >
           <Image source={studyIcon} />
           <Text style={styles.buttonText}>Estudar</Text>
         </RectButton>
